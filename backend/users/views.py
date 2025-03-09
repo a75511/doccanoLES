bd9c0c4e5668b4771ec8ff54224ace54f5d9a849
Alexandre Mendes <145663035+a75511@users.noreply.github.com>
2025-03-09 21:33:48 +0000

    UserList (#3)
    * db
    
    * Revert "db"
    
    This reverts commit 92185782c70ba7cd8b5e333c807a961581be415c.
    
    * Backend (#1)
    
    Co-authored-by: rodri17 <rodrigoguerreiro1710@gmail.com>
    
    * UserList (#2)
    
    * Backend
    
    * Frontend
    
    ---------
    
    Co-authored-by: rodri17 <rodrigoguerreiro1710@gmail.com>
    
    ---------
    
    Co-authored-by: rodri17 <rodrigoguerreiro1710@gmail.com>

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,43 +1,30 @@
-from dj_rest_auth.registration.serializers import RegisterSerializer
-from django.contrib.auth.models import User
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, generics, status
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
-from projects.permissions import IsProjectAdmin
 
+
+
+User = get_user_model()
 
 class Me(APIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         serializer = UserSerializer(request.user, context={"request": request})
         return Response(serializer.data)
 
-
 class Users(generics.ListAPIView):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated & IsProjectAdmin]
+    permission_classes = [IsAuthenticated]
     pagination_class = None
-    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
-    search_fields = ("username",)
 
+    def get_queryset(self):
+        queryset = User.objects.all()
+        search_query = self.request.query_params.get('q', None)
+        if search_query:
+            queryset = queryset.filter(username__icontains=search_query) | queryset.filter(email__icontains=search_query)
+        return queryset
 
-class UserCreation(generics.CreateAPIView):
-    serializer_class = RegisterSerializer
-    permission_classes = [IsAuthenticated & IsAdminUser]
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED, headers=headers)
-
-    def perform_create(self, serializer):
-        user = serializer.save(self.request)
-        return user
